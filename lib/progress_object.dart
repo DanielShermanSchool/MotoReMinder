d7178b6fd6c63e818857c40e3e630abad66d8169
DanielShermanSchool <dsherm12@msudenver.edu>
2023-09-28 19:10:24 -0600

    updated progress_object to include maintenance interval for use in making percentages

--- lib/progress_object.dart
+++ lib/progress_object.dart
@@ -1,15 +1,23 @@
 class progress_object {
-  int _progress = 0;
-  int _MAINTENANCE_INTERVAL = 1;
-  int get progress => _progress;
-  set progress(int value) {
-    _progress = value;
+  int _progress;
+  int _MAINTENANCE_INTERVAL;
+
+
+  progress_object({int progress = 0, int MAINTENANCE_INTERVAL = 5000})
+      : _progress = progress,
+        _MAINTENANCE_INTERVAL = MAINTENANCE_INTERVAL {
+    assert(_progress >= 0 && _progress <= _MAINTENANCE_INTERVAL);
+    assert(_MAINTENANCE_INTERVAL > 0);
   }
-  set MAINTENANCE_INTERVAL(int value) {
-    MAINTENANCE_INTERVAL = value;
-  }
-  //progress can be as high as MAINTENANCE INTERVAL
-  //progress can be as low as 0
-  //return progress as a percentage
-  double get progressPercentage => _progress / _MAINTENANCE_INTERVAL;
-}
+      int get progress => _progress;
+      set progress(int value) {
+        _progress = value;
+      }
+      set MAINTENANCE_INTERVAL(int value) {
+        _MAINTENANCE_INTERVAL = value;
+      }
+      //progress can be as high as MAINTENANCE INTERVAL
+      //progress can be as low as 0
+      //return progress as a percentage
+      double get progressPercentage => _progress / _MAINTENANCE_INTERVAL;    }
+
