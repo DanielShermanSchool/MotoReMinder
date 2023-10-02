0c6c7589359e15b8b7913b55ff98cfc1be70f39a
DanielShermanSchool <dsherm12@msudenver.edu>
2023-10-02 14:27:54 -0600

    created car object

---
+++ lib/car_object.dart
@@ -0,0 +1,10 @@
+class Car {
+  int mileage;
+  int year;
+  String make;
+  String model;
+  String trim;
+  String nickname;
+
+  Car(this.mileage, this.year, this.make, this.model, this.trim, this.nickname);
+}
