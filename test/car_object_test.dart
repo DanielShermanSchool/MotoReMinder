e72b0f0919a2709a554116e0af45ada7c6c456a4
DanielShermanSchool <dsherm12@msudenver.edu>
2023-10-18 15:05:55 -0600

    oops

--- test/car_object_test.dart
+++ test/car_object_test.dart
@@ -194,6 +194,55 @@ void main() {
   });
 
   //same test but for tire rotation
+  testWidgets('car object progress bars tire rotation', (tester) async {
+    //create car
+    Car myCar = new Car(null, 
+                        _MILEAGE,
+                        2000, 
+                        'this', 
+                        'is', 
+                        'a', 
+                        'test', 
+                        _INTERVAL_ENGING_OIL_AND_FILTER, 
+                        _INTERVAL_TIRE_ROTATION, 
+                        _INTERVAL_BRAKE_INSPECTION, 
+                        _INTERVAL_ENGINE_COOLANT, 
+                        _INTERVAL_AIR_FILTER, 
+                        _INTERVAL_SPARK_PLUG, 
+                        _INTERVAL_TIMING_BELT, 
+                        _INTERVAL_WATER_PUMP, 
+                        _INTERVAL_DRIVE_BELT, 
+                        _INTERVAL_TRANSMISSION_FLUID, 
+                        _INTERVAL_TRANSMISSION_FILTER, 
+                        _INTERVAL_BRAKE_FLUID, 
+                        _INTERVAL_CABIN_AIR_FILTER, 
+                        _INTERVAL_FUEL_FILTER, 
+                        _INTERVAL_FUEL_PUMP, 
+                        _INTERVAL_SUSPENSION, 
+                        _INTERVAL_TIRES, 
+                        _LAST_CHANGED_ENGINE_OIL_FILTER, 
+                        _LAST_CHANGED_TIRE_ROTATION, 
+                        _LAST_CHANGED_BRAKE_INSPECTION, 
+                        _LAST_CHANGED_ENGINE_COOLANT, 
+                        _LAST_CHANGED_AIR_FILTER, 
+                        _LAST_CHANGED_SPARK_PLUGS, 
+                        _LAST_CHANGED_TIMING_BELT_CHAIN, 
+                        _LAST_CHANGED_WATER_PUMP_INSPECTION, 
+                        _LAST_CHANGED_DRIVE_BELT_INSPECTION, 
+                        _LAST_CHANGED_TRANSMISSION_FLUID, 
+                        _LAST_CHANGED_TRANSMISSION_FILTER, 
+                        _LAST_CHANGED_BRAKE_FLUID, 
+                        _LAST_CHANGED_CABIN_AIR_FILTER, 
+                        _LAST_CHANGED_FUEL_FILTER, 
+                        _LAST_CHANGED_FUEL_PUMP, 
+                        _LAST_SUSPENSION_INSPECTION, 
+                        _LAST_CHANGED_TIRES);
+    
+    //test car
+    progress_object myProgress = myCar.tireRotationProgress;
+    expect(myProgress.progressPercentage, 0.5);
+  });
+
   testWidgets('car object progress bars tire rotation OVER 100%', (tester) async {
     //create car
     Car myCar = new Car(null, 
