6019faa2a5376c3bf4de6e0110e9ea480d315c3b
Gage Laframboise <kid.laframboise13@gmail.com>
2023-11-01 14:15:21 -0600

    Icon update

--- lib/pages/help_page/help_page.dart
+++ lib/pages/help_page/help_page.dart
@@ -8,14 +8,30 @@ class HelpPage extends StatelessWidget {
         title: Text('Help'),
       ),
       body: Center(
-        //child: Text('This is the Help Page'),
-        child: Text( '                                Welcome to MotoReMinder.' +
-        '\n\nTo get started tap the icon on the bottom right named new car.' +
-        '\n\nEnter all the information that you can and hit submit at the bottom of the screen.' +
-        '\n\nIf you want to edit a car than tap and hold which car you would like to edit.' +
-        '\n\nTo get to the check list tap on the car you would like to see.'
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.directions_car),
+              title: Text('Welcome to MotoReMinder.'),
+            ),
+            ListTile(
+              leading: Icon(Icons.car_repair),
+              title: Text('To get started tap the icon on the bottom right named new car.'),
+            ),
+            ListTile(
+              leading: Icon(Icons.edit),
+              title: Text('Enter all the information that you can and hit submit at the bottom of the screen.'),
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text('To get to the check list tap on the car you would like to see.'),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+
