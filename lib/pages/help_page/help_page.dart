dd77306a14fa27dbc256014e08c07b75ecd7b70f
Gage Laframboise <kid.laframboise13@gmail.com>
2023-11-08 15:36:06 -0700

    another

--- lib/pages/help_page/help_page.dart
+++ lib/pages/help_page/help_page.dart
@@ -32,7 +32,7 @@ class _HelpPageState extends State<HelpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Info'),
+        title: Text('Info Page'),
       ),
       body: SingleChildScrollView(
         child: Column(
