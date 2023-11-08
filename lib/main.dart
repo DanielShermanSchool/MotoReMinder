a8eeb0f67db98c9c9d5dd6079a656649b0fae440
Anthony Mitcham <amitcham@msudenver.edu>
2023-11-08 15:04:47 -0700

    Minor fixes/added notifications
    Adds notifications;
    minor fixes from other problems

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/material.dart';
-
-import 'index.dart';
+import 'package:moto_re_minder/pages/car_page/car_page_widget.dart';
+import 'package:moto_re_minder/pages/help_page/help_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
- runApp(MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+
+
+ 
+  
+  
+class _MyAppState extends State<MyApp> {
+
+  bool _darkModeEnabled = false;
+
+  void _onThemeChanged(bool value) {
+    setState(() {
+      _darkModeEnabled = value;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MotoReMinder',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.amber,
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: CarPageWidget(),
+      themeMode: _darkModeEnabled ? ThemeMode.dark : ThemeMode.light,
+      home: FutureBuilder(
+        future: SharedPreferences.getInstance(),
+        builder:
+            (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+          var hideHelpPage = snapshot.data!.getBool('hideHelpPage') ?? true;
+          return hideHelpPage ? CarPageWidget() : HelpPage();
+        },
+      ),
     );
   }
 }
