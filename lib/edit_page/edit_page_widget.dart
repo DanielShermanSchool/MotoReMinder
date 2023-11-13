cfd62196043416dd979669e03d959d232bc2ec32
FrederickBuker <frederickbuker@gmail.com>
2023-11-13 14:52:35 -0700

    got rid of unimporant import

--- lib/edit_page/edit_page_widget.dart
+++ lib/edit_page/edit_page_widget.dart
@@ -8,7 +8,6 @@ import 'package:moto_re_minder/index.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io' show Platform;
 
 class EditPageWidget extends StatefulWidget {
   final Car? car; //optional car object if you're editing a car
