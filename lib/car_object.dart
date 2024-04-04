b6f1fa4955e411c06115d3073b59570cc46f66f2
Shannaresh <sixinchscar@gmail.com>
2024-04-04 15:07:04 -0600

    added getters and setters and serialization

--- lib/car_object.dart
+++ lib/car_object.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/widgets.dart';
 import 'package:moto_re_minder/progress_object.dart';
@@ -11,11 +12,29 @@ class Car {
   }
 
   void loadFromXml() async {
-    final file = File('path_to_your_xml_file.xml');
+    final file = File('assets/car_attributes.xml');
     final document = XmlDocument.parse(await file.readAsString());
 
     for (var element in document.findAllElements('Car').first.children.whereType<XmlElement>()) {
       attributes[element.name.toString()] = element.text;
     }
+  }
+
+  String toJson() {
+    return jsonEncode(attributes);
+  }
+
+  static Car fromJson(String jsonString) {
+    Car car = Car();
+    car.attributes = jsonDecode(jsonString);
+    return car;
+  }
+
+  dynamic getAttribute(String name) {
+    return attributes[name];
+  }
+
+  void setAttribute(String name, dynamic value) {
+    attributes[name] = value;
   }
 }
